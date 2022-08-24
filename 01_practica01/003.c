f51a3479683f94995bb2d27c0a4e7813c020148c
kolodani <danielvinzia@gmail.com>
2022-08-24 20:46:23 -0300

    termine de acomodar todo

---
+++ 01_practica01/003.c
@@ -0,0 +1,27 @@
+/*
+    Practica 01
+    Ejercicio 03 suma de matrices
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    printf("Bienvenido a la suma de matrices NxN\n");
+    printf("primero ingrese el tamaño de la matriz:\n");
+    printf("> ");
+    scanf("%d", &n);
+    
+    int matrizA[n][n];
+    int matrizB[n][n];
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
