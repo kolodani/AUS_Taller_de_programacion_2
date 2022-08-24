d4309895cc77ab2c3c468baa5f3898343e2ea09f
kolodani <danielvinzia@gmail.com>
2022-08-24 20:59:01 -0300

    ejercico 3 de la practica 1 listo
    hecho por: kolodani

--- 01_practica01/003.c
+++ 01_practica01/003.c
@@ -16,12 +16,55 @@ int main()
     int matrizA[n][n];
     int matrizB[n][n];
     
-    
-    
-    
-    
-    
-    
-    
+    printf("Ahora ingrese los valores de la matriz A:\n");
+    for  (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("A[%d][%d] = ", i + 1, j + 1);
+            scanf("%d", &matrizA[i][j]);
+        }
+    }
+
+    printf("Ahora ingrese los valores de la matriz B:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("B[%d][%d] = ", i + 1, j + 1);
+            scanf("%d", &matrizB[i][j]);
+        }
+    }
+
+    printf("El contenido de las matrices es:\n");
+    printf("La matriz A es:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", matrizA[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("La matriz B es:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", matrizB[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("La suma de la matriz A y B es:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", matrizA[i][j] + matrizB[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
