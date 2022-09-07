774e4a1fa64e0e1bb3c561b505ea113283473bb9
kolodani <danielvinzia@gmail.com>
2022-09-07 19:44:44 -0300

    ejercicio 02 listo

--- 02_practica02/002.c
+++ 02_practica02/002.c
@@ -2,3 +2,149 @@
     Practica 02
     Ejercicio 02 Producto escalar
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Prototipo de funcion para calcular el producto escalar de una matriz
+int ** productoEscalar (int filas, int columnas, int escalar, int ** matriz);
+
+int main()
+{
+    // Declaracion de variables
+    int escalar, filas, columnas, i, j;
+    int **matriz, **matrizEscalar;
+    
+    // Solicitar el valor escalar y la dimensión de la matriz por teclado.
+    printf("Ingrese el valor escalar:\n");
+    printf("> ");
+    scanf("%d", &escalar);
+    printf("Ingrese la cantidad de filas:\n");
+    printf("> ");
+    scanf("%d", &filas);
+    printf("Ingrese la cantidad de columnas:\n");
+    printf("> ");
+    scanf("%d", &columnas);
+    
+    // Solicitar memoria para el almacenamiento.
+    matriz = (int **)malloc(filas * sizeof(int *));
+    if (matriz == NULL)
+    {
+        printf("Error al solicitar memoria para la matriz");
+        return EXIT_FAILURE;
+    }
+    for (i = 0; i < filas; i++)
+    {
+        matriz[i] = (int *)malloc(columnas * sizeof(int));
+        if (matriz[i] == NULL)
+        {
+            printf("Error al solicitar memoria para la matriz");
+            return EXIT_FAILURE;
+        }
+    }
+    
+    matrizEscalar = (int **)malloc(filas * sizeof(int *));
+    if (matrizEscalar == NULL)
+    {
+        printf("Error al solicitar memoria para la matriz");
+        return EXIT_FAILURE;
+    }
+    for (i = 0; i < filas; i++)
+    {
+        matrizEscalar[i] = (int *)malloc(columnas * sizeof(int));
+        if (matrizEscalar[i] == NULL)
+        {
+            printf("Error al solicitar memoria para la matriz");
+            return EXIT_FAILURE;
+        }
+    }
+    // Llenar la matriz con valores aleatorios.
+    for (i = 0; i < filas; i++)
+    {
+        for (j = 0; j < columnas; j++)
+        {
+            matriz[i][j] = rand() % 10;
+        }
+    }
+    
+    /*
+    // realizo el producto escalar
+    for (i = 0; i < filas; i++)
+    {
+        for (j = 0; j < columnas; j++)
+        {
+            matrizEscalar[i][j] = matriz[i][j] * escalar;
+        }
+    }
+    */
+    
+    matrizEscalar = productoEscalar(filas, columnas, escalar, matriz);
+    
+    // Imprimir la matriz
+    printf("Matriz original:\n");
+    for (i = 0; i < filas; i++)
+    {
+        for (j = 0; j < columnas; j++)
+        {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
+    // Imprimir la matriz escalar
+    printf("Matriz escalar:\n");
+    for (i = 0; i < filas; i++)
+    {
+        for (j = 0; j < columnas; j++)
+        {
+            printf("%d ", matrizEscalar[i][j]);
+        }
+        printf("\n");
+    }
+    
+    // Liberar memoria
+    for (i = 0; i < filas; i++)
+    {
+        free(matriz[i]);
+    }
+    free(matriz);
+    
+    for (i = 0; i < filas; i++)
+    {
+        free(matrizEscalar[i]);
+    }
+    free(matrizEscalar);
+    
+    return 0;
+}
+
+int ** productoEscalar (int filas, int columnas, int escalar, int ** matriz)
+{
+    int i, j;
+    int **matrizesca;
+    matrizesca = (int **)malloc(filas * sizeof(int *));
+    if (matrizesca == NULL)
+    {
+        printf("Error al solicitar memoria para la matriz");
+        return matrizesca = NULL;
+    }
+    for (i = 0; i < filas; i++)
+    {
+        matrizesca[i] = (int *)malloc(columnas * sizeof(int));
+        if (matrizesca[i] == NULL)
+        {
+            printf("Error al solicitar memoria para la matriz");
+            return matrizesca = NULL;
+        }
+    }
+    
+    for (i = 0; i < filas; i++)
+    {
+        for (j = 0; j < columnas; j++)
+        {
+            matrizesca[i][j] = matriz[i][j] * escalar;
+        }
+    }
+    
+    return matrizesca;
+}
