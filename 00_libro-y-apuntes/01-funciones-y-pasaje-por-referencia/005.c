10e4e04195b4c7dcd657c43bbc05deefb27a6005
kolodani <danielvinzia@gmail.com>
2022-12-05 17:30:40 -0300

    ejercicio de ejemplo 005 de funciones

---
+++ 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/005.c
@@ -0,0 +1,29 @@
+/* scope3.c */
+
+#include <stdio.h>
+
+void unaFuncion()
+{
+  extern int variable;
+  printf("En la funcion unaFuncion, variable es: %d\n", variable);
+}
+
+void otraFuncion()
+{
+  extern int variable;
+  variable++;
+  printf("EN la funcion otraFuncion, la variable es: %d\n", variable);
+}
+
+int main()
+{
+  extern int variable;
+  variable = 9;
+  printf("El valor de la variable es: %d\n", variable);
+  unaFuncion();
+  otraFuncion();
+  printf("Ahora el valor de variable es: %d\n", variable);
+  return 0;
+}
+
+int variable;
