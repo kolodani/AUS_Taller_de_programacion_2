5cf178b4de20f021a8c20df42ed8f6cb108dbf02
kolodani <danielvinzia@gmail.com>
2023-01-10 11:40:26 -0300

    ejercicio 003 de punteros completado

--- 00_libro-y-apuntes/02-punteros/003.c
+++ 00_libro-y-apuntes/02-punteros/003.c
@@ -4,25 +4,25 @@
 
 char *ingreso()
 {
-  char frase[256];
+  char frase[256]; // creo un arreglo grande para poder guardar la frase
   printf("Ingrese una frase: ");
 
-  scanf("%[^,`\n`]", frase);
+  scanf("%[^,`\n`]", frase); // leo la frase ingresada por teclado, lee todo hasta ingresar la tecla ENTER
 
-  char *frase1 = (char *)malloc(strlen(frase));
+  char *frase1 = (char *)malloc(strlen(frase)); // creo un puntero y le asigno el tamaño escrito del arreglo antes creado
 
-  strcpy(frase1, frase);
+  strcpy(frase1, frase); // copio el contenido de la frase
 
   return frase1;
 }
 
 int main()
 {
-  char *frase = ingreso();
+  char *frase = ingreso(); // asigno a mi puntero el resultado de la funcion
 
   printf("Frase Ingresada: %s\n", frase);
 
-  printf("Frase modificada: %s  - largo  %d\n", frase, (int)strlen(frase));
+  printf("Frase modificada: %s  - largo  %d\n", frase, (int)strlen(frase)); // muestro la frase guardada en un arreglo del tamaño justo
 
   free(frase);
 
