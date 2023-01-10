56f99cc164f5d8514b6565996f42c6f59cfe3b2c
kolodani <danielvinzia@gmail.com>
2023-01-10 18:28:12 -0300

    ejercicio 001 de la practica 01

--- 01_practica01/001.c
+++ 01_practica01/001.c
@@ -7,14 +7,14 @@
 
 int main()
 {
-    int tamano , i , j;
-    
+    int tamano, i, j; // creo las variables necesarias para el programa
+
     printf("Bienvenido a la matriz identidad\n");
     printf("Elija el tamaño de la matriz:\n");
     printf("> ");
-    scanf("%d", &tamano);
+    scanf("%d", &tamano); // guardo la variable ingresada y la guardo, si no es un numero, se genera un error
     printf("La matriz identidad de %dX%d es:\n", tamano, tamano);
-    
+
     for (i = 0; i < tamano; i++)
     {
         for (j = 0; j < tamano; j++)
@@ -30,5 +30,6 @@ int main()
         }
         printf("\n");
     }
+    // solo imprimo la matriz, en ningun momento la guardo a la misma
     return 0;
 }
