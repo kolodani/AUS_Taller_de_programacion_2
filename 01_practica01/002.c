40956b3996bb6b959693ef6bfcf36b0334187695
kolodani <danielvinzia@gmail.com>
2023-01-16 13:22:37 -0300

    ejercicio 002 de la practica 01

--- 01_practica01/002.c
+++ 01_practica01/002.c
@@ -8,37 +8,37 @@
 int main()
 {
     int materias, alumnos, i, j;
-    
+
     printf("Bienvenido a las estadisticas de notas\n");
     printf("Primero elija el la cantidad de materias que hay:\n");
     printf("> ");
-    scanf("%d", &materias);
+    scanf("%d", &materias); // cantidad de materias
     printf("Ahora elija la cantidad de alumnos:\n");
     printf("> ");
-    scanf("%d", &alumnos);
-    
-    float notas[alumnos][materias];
-    float promedioalumno[materias], promediomateria[alumnos];
-    float suma = 0.0;
-    
+    scanf("%d", &alumnos); // cantidad de alumnos
+
+    float notas[alumnos][materias];                           // matriz de notas por alumno y materia
+    float promedioalumno[materias], promediomateria[alumnos]; // vectores de promedios por alumno y materia
+    float suma = 0.0;                                         // variable para sumar resultados, variable auxiliar
+
     for (i = 0; i < alumnos; i++)
     {
         for (j = 0; j < materias; j++)
         {
             printf("Ingrese la nota del alumno %d de la materia %d: ", i + 1, j + 1);
-            scanf("%f", &notas[i][j]);
+            scanf("%f", &notas[i][j]); // ingreso de notas en la matriz creada
         }
     }
     printf("\n");
-    
+
     printf("El promedio de cada alumno es:\n");
     for (i = 0; i < alumnos; i++)
     {
         for (j = 0; j < materias; j++)
         {
-            suma += notas[i][j];
+            suma += notas[i][j]; // suma de notas por alumno
         }
-        promedioalumno[i] = suma / materias;
+        promedioalumno[i] = suma / materias; // promedio de notas por alumno
         suma = 0.0;
         printf("El promedio del alumno %d es: %.2f\n", i + 1, promedioalumno[i]);
         if (promedioalumno[i] < 6.0)
@@ -55,15 +55,15 @@ int main()
         }
         printf("\n");
     }
-    
+
     printf("El promedio de cada materia es:\n");
     for (j = 0; j < materias; j++)
     {
         for (i = 0; i < alumnos; i++)
         {
-            suma += notas[i][j];
+            suma += notas[i][j]; // suma de notas por materia
         }
-        promediomateria[j] = suma / alumnos;
+        promediomateria[j] = suma / alumnos; // promedio de notas por materia
         suma = 0.0;
         printf("El promedio de la materia %d es: %.2f\n\n", j + 1, promediomateria[j]);
     }
