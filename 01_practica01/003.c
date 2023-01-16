09f05b633e803e56b5e868eee87886a136bdc8bc
kolodani <danielvinzia@gmail.com>
2023-01-16 13:22:52 -0300

    ejercicio 003 de la practica 01

--- 01_practica01/003.c
+++ 01_practica01/003.c
@@ -11,18 +11,18 @@ int main()
     printf("Bienvenido a la suma de matrices NxN\n");
     printf("primero ingrese el tamaño de la matriz:\n");
     printf("> ");
-    scanf("%d", &n);
-    
-    int matrizA[n][n];
+    scanf("%d", &n); // ingreso el tamaño de la matriz
+
+    int matrizA[n][n]; // declaro las matrices
     int matrizB[n][n];
-    
+
     printf("Ahora ingrese los valores de la matriz A:\n");
-    for  (i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
         {
             printf("A[%d][%d] = ", i + 1, j + 1);
-            scanf("%d", &matrizA[i][j]);
+            scanf("%d", &matrizA[i][j]); // ingreso los valores de la matriz
         }
     }
 
@@ -32,10 +32,11 @@ int main()
         for (j = 0; j < n; j++)
         {
             printf("B[%d][%d] = ", i + 1, j + 1);
-            scanf("%d", &matrizB[i][j]);
+            scanf("%d", &matrizB[i][j]); // ingreso los valores de la matriz
         }
     }
 
+    printf("El tamaño de las matrices es: %d x %d\n", n, n); // imprimo el tamaño de las matrices
     printf("El contenido de las matrices es:\n");
     printf("La matriz A es:\n");
     for (i = 0; i < n; i++)
@@ -62,7 +63,7 @@ int main()
     {
         for (j = 0; j < n; j++)
         {
-            printf("%d ", matrizA[i][j] + matrizB[i][j]);
+            printf("%d ", matrizA[i][j] + matrizB[i][j]); // imprimo la suma de las matrices
         }
         printf("\n");
     }
