013a4ddd08aead5e321c33175f8dea6f203633f8
kolodani <danielvinzia@gmail.com>
2023-01-16 13:23:38 -0300

    ejercicio 007 de la practica 01

--- 01_practica01/007.c
+++ 01_practica01/007.c
@@ -10,26 +10,26 @@ int main()
     int n, i = 0;
     printf("Introduce un numero, el mismo sera el tamaño del array:\n");
     printf("> ");
-    scanf("%d", &n);
+    scanf("%d", &n); // Se pide el tamaño del array
 
-    int arreglo[n];
-    int *pa = arreglo;
+    int arreglo[n];    // Se crea el array
+    int *pa = arreglo; // Se crea un puntero al array
     do
     {
         printf("Introduce el numero de la posicion %d:\n", i + 1);
         printf("> ");
-        scanf("%d", pa);
-        pa++;
-        i++;
+        scanf("%d", pa); // Se pide el valor de la posicion i
+        pa++;            // Se incrementa el puntero
+        i++;             // Se incrementa el contador
     } while (i < n);
 
     printf("El array invertido es:\n");
     i = 0;
     do
     {
-        printf("%d\n", *(pa - 1));
-        pa--;
-        i++;
+        printf("%d\n", *(pa - 1)); // Se imprime el valor de la posicion i
+        pa--;                      // Se decrementa el puntero
+        i++;                       // Se incrementa el contador
     } while (i < n);
 
     return 0;
