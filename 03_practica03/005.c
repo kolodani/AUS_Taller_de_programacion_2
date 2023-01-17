e85410917ea754e4a83c648f50e01c37d8f9f507
kolodani <danielvinzia@gmail.com>
2023-01-17 16:45:14 -0300

    ejercicio 005 de la practica 03

--- 03_practica03/005.c
+++ 03_practica03/005.c
@@ -1,59 +1,86 @@
+/*
+    Practica 03
+    Ejercicio 05
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-typedef struct {
+// estructura de la matriz
+typedef struct
+{
     int filas;
     int columnas;
-    int ** matriz;
+    int **matriz;
 } matrix;
 
-void inicializarMatriz(matrix * m, int filas, int columnas);
-void rellenarMatriz(matrix * m);
-void mostrarMatriz(matrix * m);
-void liberarMatriz(matrix * m);
-matrix * sumaMatrices(matrix * m1, matrix * m2);
+// prototipos de las funciones
+void inicializarMatriz(matrix *m, int filas, int columnas);
+void rellenarMatriz(matrix *m);
+void mostrarMatriz(matrix *m);
+void liberarMatriz(matrix *m);
+matrix *sumaMatrices(matrix *m1, matrix *m2);
 
+// funcion principal
 int main()
 {
+    // declaracion de variables
     matrix m1, m2;
-    matrix * pm3;
-    matrix * pm1 = &m1;
-    matrix * pm2 = &m2;
+    matrix *pm3;
+    matrix *pm1 = &m1;
+    matrix *pm2 = &m2;
     int filas, columnas;
+
+    // entrada de datos
     printf("Introduzca el numero de filas: ");
     scanf("%d", &filas);
     printf("Introduzca el numero de columnas: ");
     scanf("%d", &columnas);
+
+    // inicializacion de las matrices
     inicializarMatriz(pm1, filas, columnas);
     inicializarMatriz(pm2, filas, columnas);
+    // rellenar las matrices
+
     rellenarMatriz(pm1);
     rellenarMatriz(pm2);
+
+    // mostrar las matrices
     printf("Matriz 1:\n");
     mostrarMatriz(pm1);
     printf("Matriz 2:\n");
     mostrarMatriz(pm2);
+
+    // sumar las matrices
     pm3 = sumaMatrices(pm1, pm2);
+
+    // mostrar la matriz resultante
     printf("Matriz 3:\n");
     mostrarMatriz(pm3);
+
+    // liberar la memoria
     liberarMatriz(pm1);
     liberarMatriz(pm2);
     liberarMatriz(pm3);
+
     return 0;
 }
 
-void inicializarMatriz(matrix * m, int filas, int columnas){
+void inicializarMatriz(matrix *m, int filas, int columnas)
+{
     int i;
     m->filas = filas;
     m->columnas = columnas;
-    m->matriz = (int **) malloc(filas * sizeof(int *));
+    m->matriz = (int **)malloc(filas * sizeof(int *));
     for (i = 0; i < filas; i++)
     {
-        m->matriz[i] = (int *) malloc(columnas * sizeof(int));
+        m->matriz[i] = (int *)malloc(columnas * sizeof(int));
     }
 }
 
-void rellenarMatriz(matrix * m){
+void rellenarMatriz(matrix *m)
+{
     srand(time(NULL));
     int i, j;
     for (i = 0; i < m->filas; i++)
@@ -65,7 +92,8 @@ void rellenarMatriz(matrix * m){
     }
 }
 
-void mostrarMatriz(matrix * m){
+void mostrarMatriz(matrix *m)
+{
     int i, j;
     for (i = 0; i < m->filas; i++)
     {
@@ -77,7 +105,8 @@ void mostrarMatriz(matrix * m){
     }
 }
 
-void liberarMatriz(matrix * m){
+void liberarMatriz(matrix *m)
+{
     int i;
     for (i = 0; i < m->filas; i++)
     {
@@ -86,10 +115,11 @@ void liberarMatriz(matrix * m){
     free(m->matriz);
 }
 
-matrix *sumaMatrices(matrix *m1, matrix *m2){
-    matrix * m3;
+matrix *sumaMatrices(matrix *m1, matrix *m2)
+{
+    matrix *m3;
     int i, j;
-    m3 = (matrix *) malloc(sizeof(matrix));
+    m3 = (matrix *)malloc(sizeof(matrix));
     inicializarMatriz(m3, m1->filas, m1->columnas);
     for (i = 0; i < m1->filas; i++)
     {
