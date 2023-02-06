7162a1a5359e65346d71e61f9470ab8ff0a9b218
kolodani <danielvinzia@gmail.com>
2023-02-06 17:05:11 -0300

    estructurad de datos, colaLista.c

---
+++ 00_libro-y-apuntes/04-estructura_de_datos_abstractas/colaLista.c
@@ -0,0 +1,18 @@
+/* colaLista.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "colaLista.h"
+
+/* completar las funciones prototipadas en el .h */
+
+int main()
+{
+   cola *inicio, *final;
+   inicio = NULL;
+   final = NULL;
+
+   /* Armar codigo de main para probar la cola */
+
+   return 0;
+}
