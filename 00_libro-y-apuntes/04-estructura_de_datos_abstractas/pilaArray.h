a07e234cf90a3fe06d521d91ece8812bc9e8ef8e
kolodani <danielvinzia@gmail.com>
2023-02-06 17:06:19 -0300

    estructurad de datos, pilaArray.h

---
+++ 00_libro-y-apuntes/04-estructura_de_datos_abstractas/pilaArray.h
@@ -0,0 +1,18 @@
+/* pilaArray.h */
+
+#define CAPACIDAD 15
+
+int elementos[CAPACIDAD];
+int cima = -1;
+
+voide apila(int);
+
+int desapila();
+
+void vaciaPila();
+
+int tamaño();
+
+int estaVacia();
+
+int cima();
