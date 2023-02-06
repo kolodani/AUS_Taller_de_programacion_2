fef2c7bdda925caee0962ac4240f6e36e8a2be94
kolodani <danielvinzia@gmail.com>
2023-02-06 17:03:31 -0300

    estructurad de datos, ejemplo 01

---
+++ 00_libro-y-apuntes/04-estructura_de_datos_abstractas/001.c
@@ -0,0 +1,21 @@
+/*
+   Operaciones
+*/
+
+/* agrega un elemento */
+apila(e: Elemento);
+
+/* elimina y retorna el primer elemento */
+desapila(): Elemento;
+
+/* retorna el primer elemento pero no lo elimina */
+cima(): Elemento;
+
+/* vacia la pila (pasa a tener tamaño 0) */
+vaciaPila();
+
+/* da la cantidad de elementos que posee la pila*/
+tamaño(): Entero;
+
+/* indica si la pila tiene o no elementos */
+estaVacia(): Booleano;
