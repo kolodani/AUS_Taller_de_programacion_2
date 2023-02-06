b2ea7d7d71accabe384319e77d2fb5f9bcc0e349
kolodani <danielvinzia@gmail.com>
2023-02-06 17:06:43 -0300

    estructurad de datos, pilaLista.c

---
+++ 00_libro-y-apuntes/04-estructura_de_datos_abstractas/pilaLista.c
@@ -0,0 +1,16 @@
+/* pilaLista.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilaLista.h"
+
+/* Completar las funciones prototipadas en el .h */
+
+int main()
+{
+   pila *cima;
+   cima = NULL;
+   /* Armar codigo de main para probar la pila */
+
+   return 0;
+}
