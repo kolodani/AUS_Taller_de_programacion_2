1a16daf8bccb3fe3568de53f14ab60ac99d99e29
kolodani <danielvinzia@gmail.com>
2023-02-06 17:08:35 -0300

    estructurad de datos, arbolBinario.h

---
+++ 00_libro-y-apuntes/04-estructura_de_datos_abstractas/arbolbinario.h
@@ -0,0 +1,19 @@
+/* arbolbinario.h */
+
+#define long_max 25;
+
+typedef struct nodo {
+    int dato;
+    struct nodo *h_izq;
+    struct nodo *h_der;
+}arbol;
+
+void insertar(arbol **, int);
+
+int es_miembro(arbol *, int);
+
+int suprime_minimo(arbol **);
+
+void suprime(arbol **, int);
+
+void listar(arbol *);
