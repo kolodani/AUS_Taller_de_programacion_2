cf044dfbd15eeacf50b25eec94cc16b77324c585
kolodani <danielvinzia@gmail.com>
2023-02-08 12:52:13 -0300

    funciones y pasajes por referencia, 003 actualizado

--- 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/003.c
+++ 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/003.c
@@ -6,18 +6,18 @@
 
 void imprimeValor()
 {
-  int contador;
-  contador = 5;
-  printf("El valor del contador es: %d\n", contador);
+    int contador;                                       // variable local no es la misma que la del main mas alla de llamarse igual
+    contador = 5;                                       // inicializo la variable en 5
+    printf("El valor del contador es: %d\n", contador); // muestro el valor de la variable local
 }
 
 int main()
 {
-  int contador;
-  contador = 0;
-  contador++;
-  printf("El valor del contador es: %d\n", contador);
-  imprimeValor();
-  printf("Ahora el valor de contador es: %d\n", contador);
-  return 0;
+    int contador;                                            // variable local
+    contador = 0;                                            // inicializo la variable
+    contador++;                                              // incremento el valor de la variable en 1
+    printf("El valor del contador es: %d\n", contador);      // muestro el valor de la variable
+    imprimeValor();                                          // llamo a la funcion imprimeValor
+    printf("Ahora el valor de contador es: %d\n", contador); // muestro el valor de la variable local del main
+    return 0;
 }
