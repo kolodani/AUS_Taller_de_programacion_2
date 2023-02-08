e2c1e13245a792cc01abf259c6095f3bcdb3439c
kolodani <danielvinzia@gmail.com>
2023-02-08 15:14:42 -0300

    funciones y pasajes por referencia, 009 actualizado

--- 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/009.c
+++ 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/009.c
@@ -2,24 +2,24 @@
 
 /* la funcion factorial funciona pero no se ve como la funcion matematica */
 
-#include "factorial1.h"
+#include "factorial1.h" // incluye el archivo de cabecera que contiene el prototipo de la funcion factorial
 
-int factorial (int numero)
+int factorial(int numero) // la funcion recibe un parametro de tipo entero y devuelve un entero
 {
-  int i;
-  int devuelve = 1;
-  for (i = 1; i <= numero; i++)
-  {
-    devuelve = devuelve * i;
-  }
-  return devuelve;
+    int i;                        // variable local i
+    int devuelve = 1;             // variable local devuelve inicializada en 1
+    for (i = 1; i <= numero; i++) // bucle for que se ejecuta desde 1 hasta el valor de la variable numero
+    {
+        devuelve = devuelve * i; // multiplica el valor de la variable devuelve por el valor de la variable i
+    }
+    return devuelve; // devuelve el valor de la variable devuelve
 }
 
 int main()
 {
-  int valor = 4;
-  int resultado;
-  resultado = factorial(valor);
-  printf("El factorial de %d es %d \n", valor, resultado);
-  return 0;
+    int valor = 4;                                           // variable local valor inicializada en 4
+    int resultado;                                           // variable local resultado
+    resultado = factorial(valor);                            // llama a la funcion factorial y le pasa una copia del valor de la variable valor
+    printf("El factorial de %d es %d \n", valor, resultado); // imprime el valor de valor y el valor de resultado
+    return 0;
 }
