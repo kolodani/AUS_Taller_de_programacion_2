21c79617a410f66783e63d31d7ef45930e930446
kolodani <danielvinzia@gmail.com>
2023-02-08 15:09:52 -0300

    funciones y pasajes por referencia, 008 actualizado

--- 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/008.c
+++ 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/008.c
@@ -4,17 +4,17 @@
 
 #include "referencia2.h"
 
-void modificar (int * variable)
+void modificar(int *variable) // la funcion recibe un parametro de tipo puntero a entero y no devuelve nada
 {
-  printf("\nvariable = %d dentro de modificar", *variable);
-  *variable = 9;
-  printf("\nvariable = %d dentro de modificar", *variable);
+    printf("\nvariable = %d dentro de modificar", *variable); // imprime el valor de la variable
+    *variable = 9;                                            // modifica el valor de la variable
+    printf("\nvariable = %d dentro de modificar", *variable); // imprime el valor de la variable
 }
 
 int main()
 {
-  int i = 1;
-  printf("\ni = %d antes de llamar a la funcion modificar", i);
-  modificar(&i);
-  printf("\ni = %d despues de llamar a la funcion modificar", i);
+    int i = 1;                                                      // variable local i inicializada en 1
+    printf("\ni = %d antes de llamar a la funcion modificar", i);   // imprime el valor de i
+    modificar(&i);                                                  // llama a la funcion modificar y le pasa la direccion de memoria de i
+    printf("\ni = %d despues de llamar a la funcion modificar", i); // imprime el valor de i
 }
