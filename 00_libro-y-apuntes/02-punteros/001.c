54e63eac7f18ccbdaad38b367b53aa16187ef578
kolodani <danielvinzia@gmail.com>
2023-02-08 15:20:39 -0300

    punteros, 001 actualizado

--- 00_libro-y-apuntes/02-punteros/001.c
+++ 00_libro-y-apuntes/02-punteros/001.c
@@ -5,16 +5,16 @@
 
 int main()
 {
-  int matriz[10][10], x, y; // la variable en este caso es un puntero al primer elemento, en este caso de la matriz
+    int matriz[10][10], x, y; // matriz bidimensional de 10x10 y variables locales x e y, matriz es un puntero a un puntero al primer elemento de la matriz
 
-  for (x = 0; x < 10; x++)
-  {
-    for (y = 0; y < 10; y++)
+    for (x = 0; x < 10; x++) // bucle for que se ejecuta desde 0 hasta 9 (10 veces)
     {
-      matriz[x][y] = 1 + rand() % 9;
-      printf("%d ", matriz[x][y]);
+        for (y = 0; y < 10; y++) // bucle for que se ejecuta desde 0 hasta 9 (10 veces) anidado dentro del bucle for anterior
+        {
+            matriz[x][y] = 1 + rand() % 9; // asigna un valor aleatorio entre 1 y 9 a la posicion [x][y] de la matriz
+            printf("%d ", matriz[x][y]);   // imprime el valor de la posicion [x][y] de la matriz
+        }
+        printf("\n"); // imprime un salto de linea para que la matriz se vea en forma de tabla
     }
-    printf("\n");
-  }
-  return 0;
+    return 0;
 }
