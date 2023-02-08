1c792ce8df2ad5c605ab33f514c40fc99057e03f
kolodani <danielvinzia@gmail.com>
2023-02-08 15:01:06 -0300

    funciones y pasajes por referencia, 005 actualizado

--- 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/005.c
+++ 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/005.c
@@ -4,28 +4,28 @@
 
 #include <stdio.h>
 
-void unaFuncion()
+void unaFuncion() // la funcion no recibe parametros y utiliza la variable global externa
 {
-  extern int variable;
-  printf("En la funcion unaFuncion, variable es: %d\n", variable);
+  extern int variable;                                             // variable global externa
+  printf("En la funcion unaFuncion, variable es: %d\n", variable); // imprime el valor de la variable
 }
 
-void otraFuncion()
+void otraFuncion() // la funcion no recibe parametros y utiliza la variable global externa
 {
-  extern int variable;
-  variable++;
-  printf("EN la funcion otraFuncion, la variable es: %d\n", variable);
+  extern int variable;                                                 // variable global externa
+  variable++;                                                          // incrementa el valor de la variable
+  printf("EN la funcion otraFuncion, la variable es: %d\n", variable); // imprime el valor de la variable
 }
 
 int main()
 {
-  extern int variable;
-  variable = 9;
-  printf("El valor de la variable es: %d\n", variable);
-  unaFuncion();
-  otraFuncion();
-  printf("Ahora el valor de variable es: %d\n", variable);
+  extern int variable;                                     // variable global externa
+  variable = 9;                                            // inicializa la variable
+  printf("El valor de la variable es: %d\n", variable);    // imprime el valor de la variable
+  unaFuncion();                                            // llama a la funcion unaFuncion
+  otraFuncion();                                           // llama a la funcion otraFuncion
+  printf("Ahora el valor de variable es: %d\n", variable); // imprime el valor de la variable
   return 0;
 }
 
-int variable;
+int variable; // variable global
