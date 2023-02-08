f656f1748b18e5c5e2557d97cdea3eb2edd42fc8
kolodani <danielvinzia@gmail.com>
2023-02-08 16:30:36 -0300

    punteros, 006 actualizado

--- 00_libro-y-apuntes/02-punteros/006.c
+++ 00_libro-y-apuntes/02-punteros/006.c
@@ -4,29 +4,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct
+typedef struct // defino la estructura
 {
-  char calle[25];
-  int numero;
-  char nombre[30];
-} direccion;
+    char calle[25];  // arreglo de caracteres
+    int numero;      // variable entera
+    char nombre[30]; // arreglo de caracteres
+} direccion;         // nombre de la estructura
 
 void main()
 {
-  direccion *pdir, *pdiri;
-
-  pdir = pdiri = (direccion *)malloc(1000 * sizeof(direccion));
-  strcpy(pdir->calle, "Avd. Alemania");
-  pdir->numero = 2010;
-  strcpy(pdir->nombre, "Fulano");
-
-  printf("La direccion de %s es: %s %d\n", pdir->nombre, pdir->calle, pdir->numero);
-
-  pdiri++; // pasa a la siguiente estructura
-
-  strcpy(pdiri->calle, "Avd. Pelegrini");
-  pdiri->numero = 250;
-  strcpy(pdiri->nombre, "Ingenieria");
-
-  printf("La direccion de %s es: %s %d\n", pdiri->nombre, pdiri->calle, pdiri->numero);
+    direccion *pdir, *pdiri;                                                              // declaro dos punteros a estructura
+    pdir = pdiri = (direccion *)malloc(1000 * sizeof(direccion));                         // asigno memoria para 1000 estructuras
+    strcpy(pdir->calle, "Avd. Alemania");                                                 // copio la cadena a la estructura
+    pdir->numero = 2010;                                                                  // guardo el numero en la estructura
+    strcpy(pdir->nombre, "Fulano");                                                       // copio la cadena a la estructura
+    printf("La direccion de %s es: %s %d\n", pdir->nombre, pdir->calle, pdir->numero);    // muestro la direccion
+    pdiri++;                                                                              // pasa a la siguiente estructura
+    strcpy(pdiri->calle, "Avd. Pelegrini");                                               // copio la cadena a la estructura
+    pdiri->numero = 250;                                                                  // guardo el numero en la estructura
+    strcpy(pdiri->nombre, "Ingenieria");                                                  // copio la cadena a la estructura
+    printf("La direccion de %s es: %s %d\n", pdiri->nombre, pdiri->calle, pdiri->numero); // muestro la direccion
 }
