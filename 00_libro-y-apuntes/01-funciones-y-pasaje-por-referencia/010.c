e9996989a2da861f90770eb55be0054a6a18254f
kolodani <danielvinzia@gmail.com>
2023-02-08 15:16:41 -0300

    funciones y pasajes por referencia, 010 actualizado

--- 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/010.c
+++ 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/010.c
@@ -4,17 +4,17 @@
 
 #include <stdio.h>
 
-int factorial(int numero)
+int factorial(int numero) // la funcion recibe un parametro de tipo entero y devuelve un entero
 {
-  if (numero == 1)
-    return 1;
-  else
-    return (numero * factorial(numero -1));
+    if (numero == 1)                             // si el valor de la variable numero es igual a 1
+        return 1;                                // devuelve 1
+    else                                         // si no
+        return (numero * factorial(numero - 1)); // devuelve el valor de la variable numero multiplicado por el valor de la funcion factorial con el valor de la variable numero decrementado en 1
 }
 
 int main()
 {
-  int a = 5;
-  printf("El factorial de %d es igual a: %d", a, factorial(a));
-  return 0;
+    int a = 5;                                                    // variable local a inicializada en 5
+    printf("El factorial de %d es igual a: %d", a, factorial(a)); // imprime el valor de a y el valor de factorial(a)
+    return 0;
 }
