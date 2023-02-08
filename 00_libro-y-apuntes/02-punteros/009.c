99385634d96d6fabd2d60a6dcff7a4a406f3faa0
kolodani <danielvinzia@gmail.com>
2023-02-08 16:53:33 -0300

    punteros, 009 actualizado

--- 00_libro-y-apuntes/02-punteros/009.c
+++ 00_libro-y-apuntes/02-punteros/009.c
@@ -2,31 +2,29 @@
 
 #include <stdio.h>
 
-typedef enum TiposFormas
+typedef enum TiposFormas // defino la enumeracion que es un tipo de dato que contiene un conjunto de valores constantes
 {
-  circulo,
-  cuadrado,
-  rectangulo
-} TiposFormas;
+    circulo, // creo tres constantes
+    cuadrado,
+    rectangulo
+} TiposFormas; // le doy un alias a la enumeracion
 
 int main()
 {
-  TiposFormas forma = rectangulo; // al cambiar el valor el switch mostrara los otros casos
-
-  switch (forma)
-  {
-  case circulo:
-    printf("Se eligio el circulo -> valor: %d\n", forma);
-    break;
-  case cuadrado:
-    printf("Se eligio el cuadrado -> valor: %d\n", forma);
-    break;
-  case rectangulo:
-    printf("Se eligio el rectangulo -> valor: %d\n", forma);
-    break;
-  default:
-    break;
-  }
-
-  return 0;
+    TiposFormas forma = rectangulo; // al cambiar el valor el switch mostrara los otros casos
+    switch (forma)                  // switch para mostrar el valor de la enumeracion
+    {
+    case circulo:                                                // caso del circulo
+        printf("Se eligio el circulo -> valor: %d\n", forma);    // muestro el valor de la enumeracion
+        break;                                                   // rompo el switch
+    case cuadrado:                                               // caso del cuadrado
+        printf("Se eligio el cuadrado -> valor: %d\n", forma);   // muestro el valor de la enumeracion
+        break;                                                   // rompo el switch
+    case rectangulo:                                             // caso del rectangulo
+        printf("Se eligio el rectangulo -> valor: %d\n", forma); // muestro el valor de la enumeracion
+        break;                                                   // rompo el switch
+    default:                                                     // caso por defecto vacio
+        break;                                                   // rompo el switch
+    }
+    return 0;
 }
