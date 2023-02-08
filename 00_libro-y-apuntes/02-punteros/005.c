f4ac4e2274325467031ac5467d8f1b8d3276cd72
kolodani <danielvinzia@gmail.com>
2023-02-08 16:20:03 -0300

    punteros, 005 actualizado

--- 00_libro-y-apuntes/02-punteros/005.c
+++ 00_libro-y-apuntes/02-punteros/005.c
@@ -4,21 +4,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct
+typedef struct // defino la estructura
 {
-  char calle[25];
-  int numero;
-  char nombre[30];
-} direccion;
+    char calle[25];  // arreglo de caracteres
+    int numero;      // variable de tipo entero
+    char nombre[30]; // arreglo de caracteres
+} direccion;         // nombre de la estructura
 
 void main()
 {
-  direccion *pdir;
-
-  pdir = (direccion *)malloc(sizeof(direccion));
-  strcpy(pdir->calle, "Avd. Alemania");
-  pdir->numero = 2010;
-  strcpy(pdir->nombre, "Fulano");
-
-  printf("La direccion de %s es: %s %d\n", pdir->nombre, pdir->calle, pdir->numero);
+    direccion *pdir;                                                                   // puntero a la estructura creada
+    pdir = (direccion *)malloc(sizeof(direccion));                                     // reservo memoria para la estructura
+    strcpy(pdir->calle, "Avd. Argentina");                                             // la funcion strcpy copia el contenido de la cadena de caracteres
+    pdir->numero = 2022;                                                               // asigno el valor 2010 a la variable numero
+    strcpy(pdir->nombre, "Fulano");                                                    // copio el contenido de la cadena de caracteres a la variable nombre
+    printf("La direccion de %s es: %s %d\n", pdir->nombre, pdir->calle, pdir->numero); // imprimo los datos
 }
