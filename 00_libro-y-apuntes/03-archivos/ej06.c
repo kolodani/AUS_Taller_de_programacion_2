4d88bbb09e8ffa48c76534f43528083807b26b7e
kolodani <danielvinzia@gmail.com>
2023-02-09 17:21:26 -0300

    archivos, ej06 actualizado

--- 00_libro-y-apuntes/03-archivos/ej06.c
+++ 00_libro-y-apuntes/03-archivos/ej06.c
@@ -7,26 +7,21 @@
 
 int main()
 {
-   int contador; /* recorrera en sentido inverso */
-   int letra;
-   FILE *pArchivo;
-
-   if ((pArchivo = fopen("letras.txt", "r")) == NULL)
+   int contador;                                      // variable de control
+   int letra;                                         // variable para almacenar la letra leida
+   FILE *pArchivo;                                    // puntero a archivo
+   if ((pArchivo = fopen("letras.txt", "r")) == NULL) // abre el archivo en modo lectura y verifica que no haya error
    {
-      printf("Error al abrir el archivo\n");
+      printf("Error al abrir el archivo\n"); // si hay error, muestra un mensaje
       return 0;
    }
-
-   fseek(pArchivo, 1L, SEEK_END);
-
-   for (contador = 0; contador <= 25; contador++)
+   fseek(pArchivo, 1L, SEEK_END);                 // posiciona el puntero al final del archivo
+   for (contador = 0; contador <= 25; contador++) // lee las 26 letras del archivo
    {
-      fseek(pArchivo, -2L, SEEK_CUR);
-      letra = getc(pArchivo);
-      printf("Letra leida: %c\n", letra);
+      fseek(pArchivo, -2L, SEEK_CUR);     // retrocede el puntero 2 posiciones
+      letra = getc(pArchivo);             // lee la letra
+      printf("Letra leida: %c\n", letra); // muestra la letra leida
    }
-
-   fclose(pArchivo);
-
+   fclose(pArchivo); // cierra el archivo
    return 0;
 }
