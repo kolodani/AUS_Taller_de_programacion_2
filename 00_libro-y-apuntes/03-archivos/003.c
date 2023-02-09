b90992dcfa8fd84d11ff44f407d35342fee0ca62
kolodani <danielvinzia@gmail.com>
2023-02-09 16:05:45 -0300

    archivos, 003 actualizado

--- 00_libro-y-apuntes/03-archivos/003.c
+++ 00_libro-y-apuntes/03-archivos/003.c
@@ -6,24 +6,22 @@
 
 int main()
 {
-    FILE *pArchivo, *pBinario;
-
-    pArchivo = fopen("NOMBRES.DAT", "r");
-    pBinario = fopen("NOMBRES.BIN", "wb");
-
-    if (pArchivo != NULL || pBinario != NULL)
+    FILE *pArchivo, *pBinario;                // Punteros a archivos
+    pArchivo = fopen("NOMBRES.DAT", "r");     // Abrir archivo en modo lectura "r" (read)
+    pBinario = fopen("NOMBRES.BIN", "wb");    // Abrir archivo en modo escritura "w" (write) y binario "b" (binary)
+    if (pArchivo != NULL || pBinario != NULL) // Si los archivos se abrieron correctamente
     {
-        char c;
-        while ((c = fgetc(pArchivo)) != EOF)
+        char c;                              // Variable para guardar el caracter leido
+        while ((c = fgetc(pArchivo)) != EOF) // Mientras no sea el final del archivo (EOF = End Of File) usando fgetc para leer caracter por caracter
         {
-            fputc(c, pBinario);
+            fputc(c, pBinario); // Escribir caracter en el archivo binario usando fputc
         }
-        fclose(pArchivo);
+        fclose(pArchivo); // Cerrar los archivos
         fclose(pBinario);
     }
     else
     {
-        printf("Error al abrir el archivo");
+        printf("Error al abrir el archivo"); // Si el archivo no se abrió correctamente, informo el error
     }
     return 0;
 }
