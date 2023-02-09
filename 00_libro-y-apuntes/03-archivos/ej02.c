b81330bf69829294b567dc7b8b210f213ec0ebb7
kolodani <danielvinzia@gmail.com>
2023-02-09 16:17:37 -0300

    archivos, ej02 actualizado

--- 00_libro-y-apuntes/03-archivos/ej02.c
+++ 00_libro-y-apuntes/03-archivos/ej02.c
@@ -6,24 +6,25 @@
 
 int main()
 {
-    FILE *pArchivo;
-
-    pArchivo = fopen("NOMBRES.DAT", "a");
-
-    if (pArchivo != NULL)
+    FILE *pArchivo; // Puntero a archivo
+    pArchivo = fopen("NOMBRES.DAT", "a"); // Abrir archivo en modo escritura "a" (append)
+    if (pArchivo != NULL) // Si el archivo se abrio correctamente
     {
-        fputs("Juan Roman\n", pArchivo);
+        fputs("Juan Roman\n", pArchivo); // Escribir datos en el archivo usando fputs
         fputs("Marta Rodriguez\n", pArchivo);
         fputs("Felipe Lopez\n", pArchivo);
-
-            if (fclose(pArchivo))
+        if (fclose(pArchivo)) // Si el archivo se cerro correctamente
         {
-            printf("Error al cerra el archivo");
+            printf("Error al cerra el archivo"); // Si el archivo no se cerro correctamente, informo el error
+        }
+        else
+        {
+            printf("El archivo ha sido cerrado\n"); // Se informa que el archivo se cerro correctamente
         }
     }
     else
     {
-        printf("Error al abrir archivo\n");
+        printf("Error al abrir archivo\n"); // Si el archivo no se abrio correctamente, informo el error
     }
     return 0;
 }
