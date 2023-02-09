6be2ff981e7e4820016ac4e3ac748b25ac2767f6
kolodani <danielvinzia@gmail.com>
2023-02-09 16:55:50 -0300

    archivos, ej04 actualizado

--- 00_libro-y-apuntes/03-archivos/ej04.c
+++ 00_libro-y-apuntes/03-archivos/ej04.c
@@ -2,22 +2,17 @@
 
 int main()
 {
-    FILE *fp;
-
-    char cadena[] = "Hola mundo!\n";
-
-    fp = fopen("./HM.txt", "w+");
-
-    if (fp == NULL)
+    FILE *fp; // Puntero a archivo
+    char cadena[] = "Hola mundo!\n"; // arreglo de caracteres (string), inicializado con un string
+    fp = fopen("./HM.txt", "w+"); // Abrir archivo en modo escritura y lectura "w+" (si no existe, lo crea)
+    if (fp == NULL) // Si no se pudo abrir el archivo
     {
-        printf("Error!\n");
+        printf("Error!\n"); // Imprimir mensaje de error
     }
     else
     {
-        fwrite(cadena, sizeof(char), sizeof(cadena), fp);
+        fwrite(cadena, sizeof(char), sizeof(cadena), fp); // Escribir en el archivo
     }
-
-    fclose(fp);
-
+    fclose(fp); // Cerrar archivo
     return 0;
 }
