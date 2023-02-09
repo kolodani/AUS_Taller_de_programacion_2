5fb29ee3deff0d750f9e9a8af37e8601f1e2cf4e
kolodani <danielvinzia@gmail.com>
2023-02-09 15:46:50 -0300

    archivos, 001 actualizado

--- 00_libro-y-apuntes/03-archivos/001.c
+++ 00_libro-y-apuntes/03-archivos/001.c
@@ -7,26 +7,24 @@
 
 int main()
 {
-    FILE *pArchivo; // Puntero a archivo
-
-    pArchivo = fopen("./NOMBRES.DAT", "r");
-
-    if (pArchivo != NULL)
+    FILE *pArchivo;                         // Puntero a archivo
+    pArchivo = fopen("./NOMBRES.DAT", "r"); // Abrir archivo en modo lectura "r" (read)
+    if (pArchivo != NULL)                   // Si el archivo se abrió correctamente
     {
-        char c = getc(pArchivo);
-        while (c != EOF)
+        char c = getc(pArchivo); // funcion getc() para leer el archivo caracter por caracter y almacenarlo en la variable c
+        while (c != EOF)         // Mientras no se llegue al final del archivo (EOF = End Of File)
         {
-            printf("%c", c);
-            c = getc(pArchivo);
+            printf("%c", c);    // Imprimir el caracter leído
+            c = getc(pArchivo); // Leer el siguiente caracter y almacenarlo en la variable c
         }
-        if (fclose(pArchivo))
+        if (fclose(pArchivo)) // Cerrar el archivo
         {
             printf("Error al cerrar el archivo\n");
         }
     }
     else
     {
-        printf("Error al abrir el archivo");
+        printf("Error al abrir el archivo"); // Si el archivo no se abrió correctamente, informo el error
     }
     return 0;
 }
