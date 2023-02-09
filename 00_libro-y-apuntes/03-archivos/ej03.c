857d71bd5f60a680273fd17772a43e04bebba1f3
kolodani <danielvinzia@gmail.com>
2023-02-09 16:29:56 -0300

    archivos, ej03 actualizado

--- 00_libro-y-apuntes/03-archivos/ej03.c
+++ 00_libro-y-apuntes/03-archivos/ej03.c
@@ -6,30 +6,26 @@
 
 int main()
 {
-
-    FILE *pArchivo;
-
-    char nombre[25];
-    char apellido[25];
-
-    pArchivo = fopen("./NOMBRES.DAT", "rt");
-
-    if (pArchivo != NULL)
+    FILE *pArchivo;                          // puntero a archivo
+    char nombre[25];                         // arreglo de caracteres
+    char apellido[25];                       // arreglo de caracteres
+    pArchivo = fopen("./NOMBRES.DAT", "rt"); // abre el archivo para lectura en modo texto "rt" (read text)
+    if (pArchivo != NULL)                    // si el archivo se abrio correctamente
     {
-        fscanf(pArchivo, "%s %s", nombre, apellido);
-        while (!feof(pArchivo))
+        fscanf(pArchivo, "%s %s", nombre, apellido); // lee el archivo usando fscanf
+        while (!feof(pArchivo))                      // mientras no se llegue al final del archivo (feof = end of file)
         {
-            printf("Nombre leido: %s Apellido leido: %s \n", nombre, apellido);
-            fscanf(pArchivo, "%s %s", nombre, apellido);
+            printf("Nombre leido: %s \tApellido leido: %s \n", nombre, apellido); // imprime los datos leidos
+            fscanf(pArchivo, "%s %s", nombre, apellido);                        // lee el archivo usando fscanf
         }
-        if (fclose(pArchivo))
+        if (fclose(pArchivo)) // si el archivo se cerro correctamente
         {
-            printf("Eroor al cerra el archivo\n");
+            printf("Error al cerra el archivo\n"); // si el archivo no se cerro correctamente, informo el error
         }
-        else
-        {
-            printf("Error al abrir archivo\n");
-        }
+    }
+    else
+    {
+        printf("Error al abrir el archivo\n"); // si el archivo no se abrio correctamente, informo el error
     }
     return 0;
 }
