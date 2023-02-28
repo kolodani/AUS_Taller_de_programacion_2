a78172367f92e2c8a897bfc60bda36da6bd2a0b0
kolodani <danielvinzia@gmail.com>
2023-02-28 10:20:45 -0300

    actualizando tabulacion, ejemplo 001

--- 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/001.c
+++ 00_libro-y-apuntes/01-funciones-y-pasaje-por-referencia/001.c
@@ -4,18 +4,18 @@
 
 int cubo(int base)
 {
-  int potencia;                  // variable donde se almacena el resultado
-  potencia = base * base * base; // calculo del cubo
-  return potencia;               // devuelvo el resultado
+    int potencia;                  // variable donde se almacena el resultado
+    potencia = base * base * base; // calculo del cubo
+    return potencia;               // devuelvo el resultado
 }
 
 int main()
 {
-  int numero; // variable donde se almacena el numero a calcular su cubo
+    int numero; // variable donde se almacena el numero a calcular su cubo
 
-  for (numero = 1; numero <= 5; numero++) // bucle para calcular los cubos de los numeros del 1 al 5
-  {
-    printf("El cubo de %d es igual a %d\n", numero, cubo(numero)); // imprimo el resultado llamando a la funcion cubo
-  }
-  return 0;
+    for (numero = 1; numero <= 5; numero++) // bucle para calcular los cubos de los numeros del 1 al 5
+    {
+        printf("El cubo de %d es igual a %d\n", numero, cubo(numero)); // imprimo el resultado llamando a la funcion cubo
+    }
+    return 0;
 }
