741815db1d398d3b1da857fac1757e75ac2fcfda
kolodani <danielvinzia@gmail.com>
2023-02-28 17:56:07 -0300

    actualizacion del ejercicio 009, corrigiendo variables

--- 00_libro-y-apuntes/02-punteros/009.c
+++ 00_libro-y-apuntes/02-punteros/009.c
@@ -11,20 +11,20 @@ typedef enum TiposFormas // defino la enumeracion que es un tipo de dato que con
 
 int main()
 {
-    TiposFormas forma = rectangulo; // al cambiar el valor el switch mostrara los otros casos
-    switch (forma)                  // switch para mostrar el valor de la enumeracion
+    TiposFormas forma = circulo; // al cambiar el valor el switch mostrara los otros casos
+    switch (forma)                // switch para mostrar el valor de la enumeracion
     {
-    case circulo:                                                // caso del circulo
-        printf("Se eligio el circulo -> valor: %d\n", forma);    // muestro el valor de la enumeracion
-        break;                                                   // rompo el switch
-    case cuadrado:                                               // caso del cuadrado
-        printf("Se eligio el cuadrado -> valor: %d\n", forma);   // muestro el valor de la enumeracion
-        break;                                                   // rompo el switch
-    case rectangulo:                                             // caso del rectangulo
-        printf("Se eligio el rectangulo -> valor: %d\n", forma); // muestro el valor de la enumeracion
-        break;                                                   // rompo el switch
-    default:                                                     // caso por defecto vacio
-        break;                                                   // rompo el switch
+    case circulo:                                                    // caso del circulo
+        printf("Se eligio el circulo -> valor: %d\n", forma + 1);    // muestro el valor de la enumeracion
+        break;                                                       // rompo el switch
+    case cuadrado:                                                   // caso del cuadrado
+        printf("Se eligio el cuadrado -> valor: %d\n", forma + 1);   // muestro el valor de la enumeracion
+        break;                                                       // rompo el switch
+    case rectangulo:                                                 // caso del rectangulo
+        printf("Se eligio el rectangulo -> valor: %d\n", forma + 1); // muestro el valor de la enumeracion
+        break;                                                       // rompo el switch
+    default:                                                         // caso por defecto vacio
+        break;                                                       // rompo el switch
     }
     return 0;
 }
