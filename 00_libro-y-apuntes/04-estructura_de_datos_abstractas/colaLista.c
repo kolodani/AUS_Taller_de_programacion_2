b226cdb748b5ce717ddbbfa3480677a94d70efe6
kolodani <danielvinzia@gmail.com>
2023-03-03 17:27:49 -0300

    ejercicio de cola de listas enlasadas

--- 00_libro-y-apuntes/04-estructura_de_datos_abstractas/colaLista.c
+++ 00_libro-y-apuntes/04-estructura_de_datos_abstractas/colaLista.c
@@ -4,15 +4,116 @@
 #include <stdlib.h>
 #include "colaLista.h"
 
-/* completar las funciones prototipadas en el .h */
-
 int main()
 {
    cola *inicio, *final;
    inicio = NULL;
    final = NULL;
 
-   /* Armar codigo de main para probar la cola */
-
+   encola(&inicio, &final, 10);
+   encola(&inicio, &final, 20);
+   encola(&inicio, &final, 30);
+   encola(&inicio, &final, 40);
+   encola(&inicio, &final, 50);
+   printf("El tamaño de la cola es: %d\n", tamaño(inicio));
+   printf("El primer elemento de la cola es: %d\n", frente(inicio));
+   desoncola(&inicio, &final);
+   printf("El primer elemento de la cola es: %d\n", frente(inicio));
+   desoncola(&inicio, &final);
+   printf("El primer elemento de la cola es: %d\n", frente(inicio));
+   printf("El tamaño de la cola es: %d\n", tamaño(inicio));
+   vaciaCola(&inicio);
+   printf("El tamaño de la cola es: %d\n", tamaño(inicio));
+   printf("El primer elemento de la cola es: %d\n", frente(inicio));
    return 0;
+}
+
+int frente (cola *inicio)
+{
+   if (inicio == NULL)
+   {
+      printf("La cola esta vacia\n");
+      return 0;
+   }
+   return inicio->dato;
+}
+
+int estaVacia (cola **inicio)
+{
+   if (*inicio == NULL)
+   {
+      return 0;
+   }
+   else
+   {
+      return 1;
+   }
+}
+
+int tamaño (cola *inicio)
+{
+   if (estaVacia(&inicio) == 0)
+   {
+      printf("La cola esta vacia\n");
+      return 0;
+   }
+   int cont = 0;
+   cola *aux;
+   aux = inicio;
+   while (aux != NULL)
+   {
+      cont++;
+      aux = aux->sgte;
+   }
+   return cont;
+}
+
+void vaciaCola (cola **inicio)
+{
+   if (estaVacia(inicio) == 0)
+   {
+      printf("La cola esta vacia");
+   } else {
+      cola *aux;
+      while (*inicio != NULL)
+      {
+         aux = *inicio;
+         *inicio = (*inicio)->sgte;
+         free(aux);
+      }
+   }
+}
+
+void encola(cola **inicio, cola **final, int dato)
+{
+   cola *nuevo;
+   nuevo = (cola *)malloc(sizeof(cola));
+   nuevo->dato = dato;
+   nuevo->sgte = NULL;
+   if (*inicio == NULL)
+   {
+      *inicio = nuevo;
+      *final = nuevo;
+   }
+   else
+   {
+      (*final)->sgte = nuevo;
+      *final = nuevo;
+   }
+}
+
+int desoncola(cola **inicio, cola **final)
+{
+   if (estaVacia(inicio) == 0)
+   {
+      printf("La cola esta vacia");
+      return 0;
+   }
+   int dato;
+   cola *aux;
+   aux = *inicio;
+   dato = aux->dato;
+   *inicio = (*inicio)->sgte;
+   free(aux);
+   return dato;
 }
--- 00_libro-y-apuntes/04-estructura_de_datos_abstractas/colaLista.h
+++ 00_libro-y-apuntes/04-estructura_de_datos_abstractas/colaLista.h
@@ -5,14 +5,33 @@ typedef struct nodo {
    struct nodo *sgte;
 }cola;
 
+/*
+ * que añade un elemento al final de la cola
+ */
 void encola(cola **, cola **, int);
 
+/*
+ * elimina y retorna el primer elemento de la cola.
+ * el segundo pasa a ser el primero luego de la operacion
+ */
 int desoncola(cola **, cola **);
 
-void caciaCola (cola **);
+/*
+ * vacia la cola, el tamaño pasa a ser cero
+ */
+void vaciaCola (cola **);
 
+/*
+ * retorna el tamaño de la cola, el numero de elementos
+*/
 int tamaño (cola *);
 
+/*
+ * retorna verdadero si es vacia
+*/
 int estaVacia (cola **);
 
-int frente (pila*);
+/*
+ * retorna pero no elimina el primer elemento
+*/
+int frente (cola *);
